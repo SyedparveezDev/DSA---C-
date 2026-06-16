d4c6a6ed14c5f4cef024d6f8204dc15df8778c89
SyedparveezDev <syedparveezdevlop@gmail.com>
2026-06-16 21:42:24 +0530

    new

--- First.cpp
+++ First.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
-using namespace std:
+#include <vector>
 
-void main ()
+int main() {
+    std::vector<int> numbers = {1, 2, 3, 4, 5};
+
+    std::cout << "Numbers in the vector: ";
+    for (const auto& num : numbers) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
 
